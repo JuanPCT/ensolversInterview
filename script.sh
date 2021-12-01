0a31ba06da0cb7e68ab6bebca1ea9eb4dc2cae39
reinosofrancsico <reinosofrancisco_@hotmail.com>
2021-12-01 15:44:41 -0300

    Added the .jar so it works + updated script

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@
 
 (trap 'kill 0' SIGINT;
 
-java -jar spring-boot-mysql/target/spring-boot-mysql-0.0.1-SNAPSHOT.jar &
+java -jar spring-boot-mysql-0.0.1-SNAPSHOT.jar &
 sleep 5
 cd react-frontend-springboot
 npm start
